dae47f7557bd8bced9b628d7d75b22c2432d1dc9
Mani Pavan <manipavan70@gmail.com>
2026-06-29 16:14:48 +0530

    Created first FastAPI application

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="MediVision AI API",
+    description="AI-powered X-ray fracture detection backend",
+    version="1.0.0"
+)
 
 @app.get("/")
 def home():
     return {
-        "message": "Welcome to MediVision AI Backend!"
+        "message": "Welcome to MediVision AI Backend!",
+        "status": "Running Successfully"
     }
